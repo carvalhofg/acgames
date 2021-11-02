ab6f810c34959b9e6a92bc81042cb8032e4eff8a
Gabriel Carvalho <carvalho_biel@outlook.com>
2021-11-02 20:48:39 -0300

    First Commit

---
+++ src/main/kotlin/com/acgame/gameaccountmarket/GameAccountMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.acgame.gameaccountmarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameAccountMarketApplication
+
+fun main(args: Array<String>) {
+	runApplication<GameAccountMarketApplication>(*args)
+}
---
+++ src/test/kotlin/com/acgame/gameaccountmarket/GameAccountMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acgame.gameaccountmarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameAccountMarketApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
